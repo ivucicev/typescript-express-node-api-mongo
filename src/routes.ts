f4cb72ba65b9fd9a5c94db282ce9296fb34e21fc
ivucicev <ivucicev@gmail.com>
2020-10-03 18:51:53 +0200

    fix: commit

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,7 @@
-import Router from "express";
-import { getUser, signIn, signOut, signUp } from "./controllers/user/user";
+import Router from 'express';
+import { scrapeMeBabyyy, getNames } from './controllers/user/user';
 
 export const apiRoutes = Router();
 
-// region - user
-apiRoutes.get("/user", getUser);
-apiRoutes.post("/sign-up", signUp);
-apiRoutes.post("/sign-in", signIn);
-apiRoutes.patch("/sign-out", signOut);
+apiRoutes.get('/scrape', scrapeMeBabyyy);
+apiRoutes.get('/names', getNames);
